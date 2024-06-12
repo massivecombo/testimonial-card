16788bd368db0302d3ee63347361b2e70339fd8f
Al Ocampo <aocampo34@gmail.com>
2024-06-12 17:33:01 -0400

    Updated file path for img

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/testimonial-card',
+  base: "https://massivecombo.github.io/testimonial-card/",
 })
