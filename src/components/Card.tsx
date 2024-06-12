d65efc1437f5f8e22ef915115370d84f02414d6b
Al Ocampo <aocampo34@gmail.com>
2024-06-12 18:19:10 -0400

    Added profile image to public asset folder

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,7 +3,7 @@ function Card ({name, handle, comment} : {name: string, handle: string, comment:
   return (
     <div className="w-[340px] mt-[200px] mx-4 p-6 bg-white border border-slate-200 border-solid rounded-lg shadow">
       <div className="flex flex-row">
-        <img className="w-14 h-14" src="src/assets/profile-thumbnail.png" alt="Image of profile picture for person who wrote testimonial"/>
+        <img className="w-14 h-14" src="/testimonial-card/profile-thumbnail.png" alt="Image of profile picture for person who wrote testimonial"/>
         <div className="flex flex-col justify-end ml-2">
           <h3 className="text-lg text-neutral-900 font-semibold">{name}</h3>
           <a className="text-sm text-neutral-600" href="#">{handle}</a>
