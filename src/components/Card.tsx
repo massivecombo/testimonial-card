1f5f129aa2c0e0ef01aa36834712224a2384b7e5
Al Ocampo <aocampo34@gmail.com>
2024-06-13 21:35:24 -0400

    Update some of the styling to match the design

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,15 +1,15 @@
 function Card ({name, handle, comment} : {name: string, handle: string, comment: string}) {
 
   return (
-    <div className="w-[340px] mt-[200px] mx-4 p-6 bg-white border border-slate-200 border-solid rounded-lg shadow">
+    <div className="w-[340px] mt-[200px] mx-4 p-6 bg-white border border-slate-200 border-solid rounded-lg shadow-sm">
       <div className="flex flex-row">
-        <img className="w-14 h-14" src="/testimonial-card/profile-thumbnail.png" alt="Image of profile picture for person who wrote testimonial"/>
+        <img className="w-12 h-12" src="/testimonial-card/profile-thumbnail.png" alt="Image of profile picture for person who wrote testimonial"/>
         <div className="flex flex-col justify-end ml-2">
           <h3 className="text-lg text-neutral-900 font-semibold">{name}</h3>
-          <a className="text-sm text-neutral-600" href="#">{handle}</a>
+          <a className="text-sm" href="#">{handle}</a>
         </div>
       </div>
-      <p className="text-base mt-2 text-neutral-600 ">{comment}</p>
+      <p className="text-base mt-2">{comment}</p>
     </div>
   )
 }
